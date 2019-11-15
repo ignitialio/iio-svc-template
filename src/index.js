a850c1900a8493c426397cc4421d8c6ec46a104c
Victor Andritoiu <devandrito@gmail.com>
2019-11-15 13:32:05 +0100

    better menu management

--- src/index.js
+++ src/index.js
@@ -7,30 +7,39 @@ global.iios_iiost = function(Vue) {
   // Warning: component name must be globally unique in your host app
   Vue.component('iiost', Iiost)
 
-  // EXEAMPLE
-  Vue.prototype.$services.emit('app:menu:add', [
-    {
-      path: '/myservicepath',
-      title: 'Iiost Service view',
-      icon: 'view_comfy',
-      anonymousAccess: true,
-      hideIfLogged: false,
-      route: {
-        path: '/myservicepath',
-        component: Iiost
-      }
-    }
-  ])
-
-  let onServiceDestroy = () => {
-    Vue.prototype.$services.emit('app:menu:remove', [
+  let register = () => {
+    // EXEAMPLE
+    Vue.prototype.$services.emit('app:menu:add', [
       {
-        path: '/myservicepath'
+        path: '/myservicepath',
+        title: 'Iiost Service view',
+        icon: 'view_comfy',
+        service: true,
+        anonymousAccess: true,
+        hideIfLogged: false,
+        route: {
+          path: '/service-iiost',
+          component: Iiost
+        }
       }
     ])
 
-    Vue.prototype.$services.emit('service:destroy:auth:done')
+    let onServiceDestroy = () => {
+      Vue.prototype.$services.emit('app:menu:remove', [
+        {
+          path: '/service-iiost'
+        }
+      ])
+
+      Vue.prototype.$services.emit('service:destroy:iiost:done')
+    }
+
+    Vue.prototype.$services.once('service:destroy:iiost', onServiceDestroy)
   }
 
-  Vue.prototype.$services.once('service:destroy:iios', onServiceDestroy)
+  if (Vue.prototype.$services.appReady) {
+    register()
+  } else {
+    Vue.prototype.$services.once('app:ready', register)
+  }
 }
